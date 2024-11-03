99755393cf6cf27b221baef486a7d49704f20203
Ray Heinonen <Ray_heinonen@hotmail.com>
2024-11-02 19:29:00 -0700

    Changed some comments

--- client/src/utils/exercise-utils.ts
+++ client/src/utils/exercise-utils.ts
@@ -1,12 +1,13 @@
 import { Exercise } from "../types/types";
 
+// Temporary Hardcoded data
 const exerciseTypes = {
     types: ["Chest", "Back", "Legs"],
 };
 const duration = "90";
 const intensity = "normal";
 
-// Function to get exercises from the backend. Method: GET
+// Function to get workout template from the backend. Method: GET
 export const fetchWorkoutTemplate = async (): Promise<Exercise[]> => {
     const response = await fetch(
         `http://localhost:8080/workout-template?types=${exerciseTypes.types}&duration=${duration}&intensity=${intensity}`
