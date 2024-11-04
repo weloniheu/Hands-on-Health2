bcf4ebd86dd40df3cc819806bf81fa81729c0327
guguji3 <86742001+guguji3@users.noreply.github.com>
2024-11-03 21:53:01 -0800

    Duration Page done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,41 +1,22 @@
 import React from "react";
-import "./App.css";
-import { fetchWorkoutTemplate } from "./utils/exercise-utils";
-import { Exercise } from "./types/types";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./HomePage";
+import DurationSelectionPage from "./DurationSelectionPage";
+import FocusPage from "./FocusPage";
 
-// ALL TEMPORARY CODE - CAN BE DELETED
 function App() {
-    const [exercises, setExercises] = React.useState<Exercise[]>([]);
-    const [show, setShow] = React.useState<boolean>(false);
-
     return (
-        <div className="App">
-            <button
-                onClick={() => {
-                    fetchWorkoutTemplate().then((data) => {
-                        setExercises(data);
-                        setShow(true);
-                    });
-                }}
-            >
-                Test
-            </button>
-            <ul>
-                {show
-                    ? exercises.map((exercise, index) => {
-                          return (
-                              <li key={index}>
-                                  <h2>{exercise.name}</h2>
-                                  <p>Type: {exercise.type}</p>
-                                  <p>Sets: {exercise.sets}</p>
-                                  <p>Reps: {exercise.reps}</p>
-                              </li>
-                          );
-                      })
-                    : null}
-            </ul>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<HomePage />} /> {/* Home Page Route */}
+                    <Route path="/select-duration" element={<DurationSelectionPage />} /> {/* Duration Selection Page */}
+                    <Route path="/focus" element={<FocusPage />} /> {/* Focus Page */}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
 export default App;
+
---
+++ client/src/FocusPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FocusPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Focus Page</h1>
+        </div>
+    );
+};
+
+export default FocusPage;
