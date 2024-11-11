614b79ed9e5a41ee3cfaf47505885dc13e7ffe6c
Ray Heinonen <Ray_heinonen@hotmail.com>
2024-11-10 17:36:43 -0800

    Update App.test.tsx

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,11 +1,2 @@
-import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
-import App from "./App";
-import { AppProvider } from "./contexts/AppContext";
 
-test("renders Welcome to Hands on Health", () => {
-  render(<App />);
-  const welcomeElement = screen.getByText(/Welcome to Hands on Health/i);
-  expect(welcomeElement).toBeInTheDocument();
-});
 
