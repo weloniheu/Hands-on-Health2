8a3a9f45e2a5134bd8152ed568d31a8e24bb2423
weloniheu <weloniheu@gmail.com>
2024-11-10 16:35:16 -0800

    deleting a set default code

--- client/src/componants/EditWorkOutPlan/ExerciseList.tsx
+++ client/src/componants/EditWorkOutPlan/ExerciseList.tsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { AppContext } from "../../contexts/AppContext";
 import { defaultAvaliableExercises } from "../../constants/Initial_consts";
 import DeleteExercise from "./DeleteExercise";
+import DeleteSet from "./DeleteSet";
 import { Exercise2 } from "../../types/types";
 
 const ExerciseList = () => {
@@ -15,15 +16,47 @@ const ExerciseList = () => {
 
   //TODO Function to load the current list of workouts from the Backend
 
+  // return (
+  //   <ul className="list-availableExercises">
+  //     {AvailableExercises.map((Exercise) => (
+  //       <div className="exercise-box">
+  //         <h2>{Exercise.name}</h2>
+  //         <DeleteExercise Exercise={Exercise} />
+  //       </div>
+  //     ))}
+  //   </ul>
+  // );
+
+  const handleUpdateExercise = (updatedExercise: Exercise2) => {
+    const updatedExercises = AvailableExercises.map((ex) =>
+        ex.name === updatedExercise.name ? updatedExercise : ex
+    );
+    setAvailableExercises(updatedExercises);
+  };
+
   return (
-    <ul className="list-availableExercises">
-      {AvailableExercises.map((Exercise) => (
-        <div className="exercise-box">
-          <h2>{Exercise.name}</h2>
-          <DeleteExercise Exercise={Exercise} />
-        </div>
-      ))}
-    </ul>
+      <ul className="list-availableExercises">
+        {AvailableExercises.map((exercise) => (
+            <div key={exercise.name} className="exercise-box">
+              <h2>{exercise.name}</h2>
+              <p>Type: {exercise.type}</p>
+              <h3>Sets:</h3>
+              <ul>
+                {exercise.sets.map((set, index) => (
+                    <li key={index}>
+                      Set {index + 1}: Reps - {set.reps}, Weight - {set.weight}
+                      <DeleteSet
+                          exercise={exercise}
+                          setIndex={index}
+                          onUpdateExercise={handleUpdateExercise}
+                      />
+                    </li>
+                ))}
+              </ul>
+              <DeleteExercise Exercise={exercise} />
+            </div>
+        ))}
+      </ul>
   );
 };
 
