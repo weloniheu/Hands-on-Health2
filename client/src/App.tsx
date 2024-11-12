beb8c50e674a4266cd8110ff321c6e25a43de25b
weloniheu <weloniheu@gmail.com>
2024-11-12 14:34:19 -0800

    working on css for CurrectWorkout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,25 +11,38 @@ import FocusMusclesView from "./views/FocusMuscles";
 import Review from "./components/WorkOutPlan/review";
 import CurrentWorkout_AddExercise_Combined from "./views/CurrentWorkOut_AddExercise_CombinedView";
 
+// function App() {
+//     return (
+//         <AppProvider>
+//             <WorkoutProvider>
+//                 <Router>
+//                     <Routes>
+//                         <Route path="/" element={<HomePage />} />
+//                         <Route path="/select-duration" element={<DurationSelectionPage />} />
+//                         <Route path="/select-focus" element={<FocusMusclesView />} />
+//                         <Route path="/select-intensity" element={<IntensitySelectionPage />} />
+//                         <Route path="/review-plan" element={<Review />} />
+//                         {/* <Route path="/review-workout" element={<ReviewWorkoutPage />} /> */}
+//                         <Route path="/current-workout" element={<CurrentWorkout}/>
+//                         {/* <Route path="/focus" element={<FocusPage />} /> */}
+//                     </Routes>
+//                 </Router>
+//             </WorkoutProvider>
+//         </AppProvider>
+//     );
+// }
+
+// App.tsx
+
 function App() {
     return (
-        <AppProvider>
-            <WorkoutProvider>
-                <Router>
-                    <Routes>
-                        <Route path="/" element={<HomePage />} />
-                        <Route path="/select-duration" element={<DurationSelectionPage />} />
-                        <Route path="/select-focus" element={<FocusMusclesView />} />
-                        <Route path="/select-intensity" element={<IntensitySelectionPage />} />
-                        <Route path="/review-plan" element={<Review />} />
-                        {/* <Route path="/review-workout" element={<ReviewWorkoutPage />} /> */}
-                        <Route path="/current-workout" element={<CurrentWorkout_AddExercise_Combined />} />
-                        {/* <Route path="/focus" element={<FocusPage />} /> */}
-                    </Routes>
-                </Router>
-            </WorkoutProvider>
-        </AppProvider>
+        <div className="App">
+            <main>
+                <CurrentWorkout_AddExercise_Combined/>
+            </main>
+        </div>
     );
 }
+
 
 export default App;
