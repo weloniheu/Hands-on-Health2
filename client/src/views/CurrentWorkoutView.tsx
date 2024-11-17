e18ef34123dfe9349e31305860c6901f92ebf771
SmoooothOperator <roz003@ucsd.edu>
2024-11-17 15:20:12 -0800

    updated html for current workout page

--- client/src/views/CurrentWorkoutView.tsx
+++ client/src/views/CurrentWorkoutView.tsx
@@ -3,37 +3,59 @@ import "./css/CurrentWorkoutView.css";
 import { AppContext } from "../contexts/AppContext";
 
 interface CurrentWorkoutProps {
-    onAddExercise: () => void
+  onAddExercise: () => void;
 }
 
-export const CurrentWorkout: React.FC<CurrentWorkoutProps> = ({ onAddExercise }) => {
+export const CurrentWorkout: React.FC<CurrentWorkoutProps> = ({
+  onAddExercise,
+}) => {
   const { currentWorkoutExercises } = useContext(AppContext);
 
-    return (
-      <div>
-        <div className="header-container">
-          <h1 className="header-title">Current Workout</h1>
-          <button className="finish-exercise" >Finish Exercise</button>
-        </div>
-        <div className="exercise-list-container">
+  return (
+    <div>
+      <div className="header-container">
+        <h1 className="header-title">Current Workout</h1>
+        <button className="finish-exercise">Finish Exercise</button>
+      </div>
+      <div className="exercise-list-container">
         {currentWorkoutExercises.map((exercise, index) => (
-          <div key={index} className="exercise-item">
-            <h2>{exercise.type}</h2>
-            <h2>{exercise.name}</h2>
-            {exercise.sets.map((set, setIndex) => (
-              <div key={setIndex}>
-                <p>Weight: {set.weight}</p>
-                <p>Reps: {set.reps}</p>
-                <button className="edit-exercise-item">Edit</button>
-                <button className="delete-exercise-item" >Delete</button>
+          <div
+            key={index}
+            className="exercise-item-container"
+          >
+            <div className="exercise-type-container">
+              <h1>{exercise.type + ":"}</h1>
+            </div>
+            <div className="exercise-item">
+              <div className="exercise-name-buttons-container">
+                <h2>{exercise.name}</h2>
+                <div className="exercise-button-holder">
+                  <button className="add-set-button">Add Set</button>
+                  <button className="delete-exercise-item">Delete</button>
+                  <button className="demo-button">Demo</button>
+                  <button className="edit-exercise-item">Edit</button>
+                </div>
               </div>
-            ))}
+              {exercise.sets.map((set, setIndex) => (
+                <div key={setIndex}>
+                  <div className="display-sets">
+                    <p>Weight: {set.weight}</p>
+                    <p>Reps: {set.reps}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         ))}
-        </div>
-        <div>
-        <button className="add-exercise-button" onClick={onAddExercise}>Add Exercise</button>        
-        </div>
       </div>
-    ); 
-}
+      <div>
+        <button
+          className="add-exercise-button"
+          onClick={onAddExercise}
+        >
+          Add Exercise
+        </button>
+      </div>
+    </div>
+  );
+};
