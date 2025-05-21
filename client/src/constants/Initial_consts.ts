befda4cb27c17827b1773ed4286b4b8ab59c4b76
weloniheu <weloniheu@gmail.com>
2025-05-21 12:40:59 -0700

    new api for static website

--- client/src/constants/Initial_consts.ts
+++ client/src/constants/Initial_consts.ts
@@ -22,4 +22,5 @@ export const defaultAvaliableExercises = [
   { name: "Deadlifts", type: "Back", sets: defaultSets },
 ];
 
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = 
+  process.env.REACT_APP_API_URL || "http://localhost:8080";
