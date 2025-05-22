fc2bc772c51f6bbbd2c4309c9b8db8237c3232ff
weloniheu <weloniheu@gmail.com>
2025-05-21 17:31:59 -0700

    returning to render

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,25 +17,25 @@ app.use("/exercise", exerciseRoute);
 app.use("/auth", authRoutes);
 app.use("/exercises", workoutPlanRoute)
 
-// app.get("/test", (req, res) => {
-// 	res.send("Test route working!");
-// });
-
-
-// Serve static files from React
-app.use(express.static(path.join(__dirname, "../../client/build")));
-
-// Catch-all: send index.html for any unmatched route (for React Router)
-app.get("*", (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
+app.get("/test", (req, res) => {
+	res.send("Test route working!");
 });
 
 
-// // Root endpoint to get test if the server is running
-// app.get("/", (req: Request, res: Response) => {
-// 	res.send({ data: "Hello, TypeScript Express!" });
-// 	res.status(200);
+// // Serve static files from React
+// app.use(express.static(path.join(__dirname, "../../client/build")));
+
+// // Catch-all: send index.html for any unmatched route (for React Router)
+// app.get("*", (req: Request, res: Response) => {
+//   res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
 // });
+
+
+// Root endpoint to get test if the server is running
+app.get("/", (req: Request, res: Response) => {
+	res.send({ data: "Hello, TypeScript Express!" });
+	res.status(200);
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
